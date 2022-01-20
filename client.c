865d736d13ef967e7a2e9400cb550566b6e37eec
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-01-20 16:41:08 +0100

    The client can send a signal now

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 22:15:49 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/01/20 01:41:44 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/01/20 13:49:12 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 #include <sys/types.h> /* various type definitions, like pid_t */
 #include <signal.h>
 #include "./libft/libft.h"
-//#include "./ft_printf/ft_printf.h"
+#include "./ft_printf/ft_printf.h"
 
 int	main(int c, char **v)
 {
 	int		server_pid;
-	
+
 	if (c != 3)
-		return (printf("You need to pass Server's PID and the message as arguments!\n"), 0);
+	{
+		ft_printf("You need to just pass Server's PID and the message as arguments!\n");
+		exit(1);
+	}
 	server_pid = ft_atoi(v[1]);
-	printf("Messgae sent to: %d\n", server_pid);
+	ft_printf("Messgae sent to: %d\n", server_pid);
 	kill(server_pid, SIGUSR1);
 }
