dc904983040c693b344ee89c105a5c9b88cf3af9
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-01-21 20:08:21 +0100

    Starting with bonus part

--- client_bonus.c
+++ client_bonus.c
@@ -6,7 +6,72 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 03:02:41 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/01/21 03:02:42 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/01/21 19:39:23 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/types.h>
+#include <signal.h>
+#include "./libft/libft.h"
+#include "./ft_printf/ft_printf.h"
+
+void	confermation_signal(int signum)
+{
+	ft_printf("The message sent successfully to the server!!");
+}
+
+void	char_to_bits(char c, pid_t pid)
+{
+	unsigned int	bit;
+
+	bit = -1;
+	while (++bit < 32)
+	{
+		if (1 & (c >> bit))
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				ft_printf("Error while sending the signal!\n");
+				exit(1);
+			}
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				ft_printf("Error while sending the signal!\n");
+				exit(1);
+			}
+		}
+		usleep(200);
+	}
+	pause();
+	signal(SIGUSR1, &confermation_signal);
+}
+
+void	send_signal(pid_t pid, char *msg)
+{
+	int	i;
+
+	i = -1;
+	while (msg[++i])
+		char_to_bits(msg[i], pid);
+}
+
+int	main(int c, char **v)
+{
+	int		server_pid;
+
+	if (c != 3)
+	{
+		ft_printf("Pass only Server's PID and the message as arguments!\n");
+		exit(1);
+	}
+	server_pid = ft_atoi(v[1]);
+	if (server_pid <= 0)
+	{
+		ft_printf("Server's PID can't be equals to or less than 0!\n");
+		exit(1);
+	}
+	send_signal(server_pid, v[2]);
+}
--- server_bonus.c
+++ server_bonus.c
@@ -6,7 +6,53 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 03:02:29 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/01/21 03:02:30 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/01/21 19:35:29 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/types.h>
+#include <signal.h>
+#include "./libft/libft.h"
+#include "./ft_printf/ft_printf.h"
+
+void	sig_handler(int signum, siginfo_t *siginfo, void *context)
+{
+	static int	i;
+	static char	c;
+	static int	server_pid;
+	static int	old_server_pid;
+
+	context = NULL;
+	server_pid = siginfo->si_pid;
+	if (server_pid != old_server_pid)
+	{
+		i = 0;
+		c = 0;
+	}
+	old_server_pid = server_pid;
+	if (signum == SIGUSR1)
+		c = (1 << i) | c;
+	if (i == 6)
+	{
+		ft_printf("%c", c);
+		i = 0;
+		c = 0;
+	}
+	else
+		i++;
+}
+
+int	main(void)
+{
+	pid_t				my_pid;
+	struct sigaction	sa_h;
+	
+	sa_h.sa_flags = SA_SIGINFO;
+	sa_h.sa_sigaction = &sig_handler;
+	sigaction(SIGUSR1, &sa_h, NULL);
+	sigaction(SIGUSR2, &sa_h, NULL);
+	my_pid = getpid();
+	ft_printf("Server's PID: %d\n", my_pid);
+	while (1)
+		pause();
+}
